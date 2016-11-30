ec33d5260633754446208b749d5169b3ef143dc6
maimitp9@gmail.com <maimitp9@gmail.com>
2016-11-30 17:44:19 +0530

    change email address in devlopment.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,8 +57,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost:3000'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    user_name:      'maimitp9@gmail.com',
-    password:       'maimit@131913920',
+    user_name:      'maimittest@gmail.com',
+    password:       'maimittest@123',
     domain:         'localhost:3000',
     address:       'smtp.gmail.com',
     port:          '587',
